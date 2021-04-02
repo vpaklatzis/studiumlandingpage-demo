2cb8254a71035594c6c334c2a0f49fc4d47f56c4
Vangelis <vpaklatzis@gmail.com>
2021-04-02 23:50:02 +0300

    Added Sign in part 1

---
+++ src/components/Signin/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { 
+    FormButton, 
+    FormContent, 
+    FormInput, 
+    FormLabel, 
+    FormWrap, 
+    Container, 
+    Icon, 
+    Form, 
+    FormH1, 
+    Text 
+} from './SigninElements';
+
+const SignIn = () => {
+    return (
+        <>
+            <Container>
+                <FormWrap>
+                    <Icon to="/">Studium</Icon>
+                    <FormContent>
+                        <Form action='#'>
+                            <FormH1>Sign in to your account</FormH1>
+                            <FormLabel htmlFor='for'>Email</FormLabel>
+                            <FormInput type='email' required />
+                            <FormLabel htmlFor='for'>Password</FormLabel>
+                            <FormInput type='password' required />
+                            <FormButton type='submit'>Continue</FormButton>
+                            <Text>Forgot password?</Text>
+                        </Form>
+                    </FormContent>
+                </FormWrap>
+            </Container>
+        </>
+    )
+}
+
+export default SignIn;
